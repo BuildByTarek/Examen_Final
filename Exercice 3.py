803036ade7cc33bd51bd624ac150ef3ebd1c5ff9
Tarek-H <hamrounitarek11@gmail.com>
2026-01-15 15:06:35 -0500

    Création de Methode saisir sur Exercice 3.py

--- Exercice 3.py
+++ Exercice 3.py
@@ -1,3 +1,9 @@
 class Outils:
     def __init__(self):
         self.nombres = []
+
+    def saisir(self):
+        self.nombres=[]
+        for i in range (10):
+            n = int (input ("Tape l'entier numeroo" + str(i+1) +": "))
+            self.nombre.append(n)
