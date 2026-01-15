22be26aff23387ce71c6b8aa0c2c22e504f5faec
Tarek-H <hamrounitarek11@gmail.com>
2026-01-15 17:29:25 -0500

    créer la methode sauver pour Exercice 2.py

--- Exercice 2.py
+++ Exercice 2.py
@@ -2,5 +2,25 @@ import os
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget,QPushButton,QGridLayout, QFrame, QLineEdit, QLabel
 
-Liste =[]
+liste =[]
 scores= []
+
+def charger():
+    f= open("Resultats.txt", "r")
+    for l in liste:
+        l.setText(f.readline())
+    f.close()
+
+def sauver():
+    f= open("Resultats.txt", "w")
+    for l in liste:
+        f.write(l.text()+"\n")
+    f.close()
+
+
+
+
+
+
+
+
