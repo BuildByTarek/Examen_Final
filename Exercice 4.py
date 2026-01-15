7f949560c71d67ee07c740f27c9a8a078d78ea22
Tarek-H <hamrounitarek11@gmail.com>
2026-01-15 13:46:19 -0500

    création de methode fonctionne sur Exercice 4.py

--- Exercice 4.py
+++ Exercice 4.py
@@ -2,3 +2,6 @@ class Voiture:
     def __init__(self,marque, annee):
         self.marque = marque
         self.annee = annee
+
+    def fonctionne(self):
+        print(f"La voiture {self.marque} fonctinne bien")
