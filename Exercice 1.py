5e4ab966d56d9ac6a0a87550aefeb68ae8216bc7
Tarek-H <hamrounitarek11@gmail.com>
2026-01-15 16:40:53 -0500

    creation de methode ligne gauche Exercice 1.py

--- Exercice 1.py
+++ Exercice 1.py
@@ -1,3 +1,8 @@
 class Triangle:
     def __init__(self, n):
         self.n = n
+
+    def ligne_gauche(self, i):
+        espaces = " " * (self.n - i)
+        etoiles = "*" * i
+        return espaces + etoiles
