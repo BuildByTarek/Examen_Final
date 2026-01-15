affff32893212244f08dd335012870512303629e
Tarek-H <hamrounitarek11@gmail.com>
2026-01-15 15:31:50 -0500

    Création de Methode minimum sur Exercice 3.py

--- Exercice 3.py
+++ Exercice 3.py
@@ -6,4 +6,12 @@ class Outils:
         self.nombres=[]
         for i in range (10):
             n = int (input ("Tape l'entier numeroo" + str(i+1) +": "))
-            self.nombre.append(n)
+            self.nombres.append(n)
+
+    def minimum (self):
+        petit = self.nombres[0]
+        for n  in self.nombres:
+            if n< petit:
+                petit =n
+
+        return petit
